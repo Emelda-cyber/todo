fcd0e939f9127a6484584161b09c14a83ee6ff49
Emelda-cyber <emeldaadwoa2002@gmail.com>
2024-12-02 14:57:27 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyFirstTodoComponent } from "./my-first-todo/my-first-todo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyFirstTodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
